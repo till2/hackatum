45d0cb41e18a0d06eb6cc32e1d7df80f2472a6f6
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2024-11-23 09:49:03 +0100

    color changes and first MapsAndOptions

---
+++ frontend/src/components/MapsAndOptions.tsx
@@ -0,0 +1,13 @@
+import Maps from "./Maps";
+import Options from "./Options";
+import "./MapsAndOptions.css";
+
+const MapsAndOptions = () => {
+    return (
+        <div className="mapsAndOptions">
+            <Maps />
+            <Options />
+        </div>
+    );
+};
+export default MapsAndOptions;
