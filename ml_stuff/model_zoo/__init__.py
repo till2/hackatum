760ab0cd3da36211890985e61236b4f1ab7c9e42
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2024-11-24 10:03:09 +0100

    Delete not needed files

--- ml_stuff/model_zoo/__init__.py
+++
@@ -1,7 +0,0 @@
-from .advanced_unets import *
-from .axial_vit import AxialVisionTransformer
-from .mlp import MLP
-from .sam import SAM
-from .stage1_custom_cnn import AttentionCNN
-from .swin_transformer import SwinTransformer, SwinUnet
-from .mic import MIC
